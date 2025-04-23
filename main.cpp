4304b4257610872e87b26b919623490ca1965c34
Esten Readel <dragonkeeper0419@gmail.com>
2025-04-22 19:50:50 -0500

    Comments added and cout's removed

--- main.cpp
+++ main.cpp
@@ -7,72 +7,63 @@ class HopcroftKarp
 private:
     int m, n; // Number of vertices in left and right columns
     std::vector<std::vector<int> > adj; // Adjacency list, 1-indexed
-    std::vector<int> pairU, pairV, dist; // Pairing and distances
+    std::vector<int> pairU, pairV, dist; // Pairing and distances, 1-indexed
 
-    bool bfs()
+    bool bfs() // Breadth-First Search
     {
         std::queue<int> q;
 
 
-        for (int u = 1; u <= m; u++)
+        for (int u = 1; u <= m; u++) // Iterates over all verticies in U/left column
         {
-            if (pairU[u] == 0)
+            if (pairU[u] == 0) // If u has no pairing
             {
-                dist[u] = 0;
-                q.push(u);
+                dist[u] = 0; // Set distance to 0
+                q.push(u); // Add u to the queue
             } else
             {
-                dist[u] = INT_MAX;
+                dist[u] = INT_MAX; // Set distance to infinity
             }
         }
-        dist[0] = INT_MAX;
+        dist[0] = INT_MAX; // Set distance to the dummy node to infinity
 
-        while (q.size() > 0)
+        while (q.size() > 0) // While queue is not empty
         {
-            int u = q.front();
+            int u = q.front(); // Get the front element
             q.pop();
 
-            if (dist[u] < dist[0])
+            if (dist[u] < dist[0]) // If distance the distance of u is less than the current shortest augmenting path
             {
-                for (int v: adj[u])
+                for (int v: adj[u]) // for each vertex v in the adjacency list of u
                 {
-                    if (dist[pairV[v]] == INT_MAX)
+                    if (dist[pairV[v]] == INT_MAX) // If the node v is matched to has not been visited
                     {
-                        dist[pairV[v]] = dist[u] + 1;
-                        q.push(pairV[v]);
+                        dist[pairV[v]] = dist[u] + 1; // set the distance to the node v is matched to, to the distance of u, plus 1
+                        q.push(pairV[v]); // add the node v is matched to, to the queue
                     }
                 }
             }
         }
-        std::cout << "finished bfs : " << (dist[0] != INT_MAX) << std::endl;
-        return dist[0] != INT_MAX;
+        return dist[0] != INT_MAX; // If the distance of the dummy node is not infinity, then there is an augmenting path, otherwise it is maximal
     }
 
-    bool dfs(int u)
+    bool dfs(int u) // Depth-First Search
     {
-        if (u == 0) return true;
+        if (u == 0) return true; // If u is the dummy node, return true
 
-        for (int v : adj[u])
+        for (int v : adj[u]) // for each vertex v in the adjacency list of u
         {
-            // int v = *i;
-            // if (pairV[v] == 0)
-            // {
-            //     pairV[v] = u;
-            //     pairU[u] = v;
-            //     return true;
-            // }
-
-            if (dist[pairV[v]] == dist[u] + 1)
+            if (dist[pairV[v]] == dist[u] + 1) // If the distance of the node v is matched to is equal to the distance of u, plus 1
             {
-                if (dfs(pairV[v]))
+                if (dfs(pairV[v])) // recursive, if the node v is matched to can be matched to another node
                 {
-                    pairV[v] = u;
-                    pairU[u] = v;
+                    pairV[v] = u; // set the node v is matched to, to u
+                    pairU[u] = v; // set the node u is matched to, to v
                     return true;
                 }
             }
         }
-        dist[u] = INT_MAX;
+        dist[u] = INT_MAX; // if no augmenting path is found, set the distance of u to infinity
         return false;
     }
 
@@ -95,11 +86,11 @@ public:
     int maxMatching()
     {
         int matching = 0;
-        while (bfs())
+        while (bfs()) // While there is at least one augmenting path
         {
-            for (int u = 1; u <= m; u++)
+            for (int u = 1; u <= m; u++) // Iterate over all vertices in U/left column
             {
-                if (pairU[u] == 0 && dfs(u))
+                if (pairU[u] == 0 && dfs(u)) // If u has no pairing and there is an augmenting path
                 {
                     matching++;
                 }
