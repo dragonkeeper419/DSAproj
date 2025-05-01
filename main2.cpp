e327dad45e7957b442aed56fa3d8efedf083be56
Tonez3D <111995183+Tonez3D@users.noreply.github.com>
2025-05-01 16:10:29 -0500

    Done I Think

--- main2.cpp
+++ main2.cpp
@@ -4,14 +4,10 @@
 
 // Prim's Algorithm in C++
 
-#include <cstring>
 #include <iostream>
 #include <vector>
-#include <list>
 
 using namespace std;
-
-#define INF 9999999
 
 void printMST(vector<int> &parent, vector<vector<int>> &graph) {
     cout << "Edge \tWeight\n";
@@ -21,62 +17,49 @@ void printMST(vector<int> &parent, vector<vector<int>> &graph) {
 }
 
 int minKey(vector<int> &key, vector<bool> &mstSet) {
-
-    // Initialize min value
-    int min = INT_MAX, min_index;
+    int min = INT_MAX, min_index; // Initialize min value
 
     for (int v = 0; v < mstSet.size(); v++)
-        if (mstSet[v] == false && key[v] < min)
+        if (!mstSet[v] && key[v] < min) { // If the node has not been visited and the edge weight is less than the current min
             min = key[v], min_index = v;
-
+        }
     return min_index;
 }
 
 void prim_algo(std::vector<std::vector<int>> &graph) {
     int vertex_count = graph.size();
-
-    vector<int> parent(vertex_count);
-
-    vector<int> next_branch(vertex_count);
-
-    vector<bool> visted(vertex_count);
-
+    vector<int> MST(vertex_count); // Contains the MST branches
+    vector<int> node_query(vertex_count); // Contains the nodes connect to those in the MST
+    vector<bool> visted(vertex_count); // Tracks what nodes have been visited
     for (int i = 0; i < vertex_count; i++)
     {
-        next_branch[i] = INT_MAX;
-        visted[i] = false;
+        node_query[i] = INT_MAX; //Setting all nodes as INF far away
+        visted[i] = false; //Setting all nodes as unvisited
     }
-
-    next_branch[0] = 0;
-
-    parent[0] = -1;
-
+    node_query[0] = 0; //Setting node 0 to be the first node we look at
+    MST[0] = -1; //Setting to node 0 to be the first node of the MST
     for (int count = 0; count < vertex_count - 1; count++) {
-
-        int u = minKey(next_branch, visted);
-
-        visted[u] = true;
-
+        int u = minKey(node_query, visted); //Finding the adjacent node with the least weight
+        visted[u] = true; //Setting the node as visited, so we don't look cycle back to it
         for (int v = 0; v < vertex_count; v++)
         {
-            if (graph[u][v] && visted[v] == false && graph[u][v] < next_branch[v])
+            if (graph[u][v] && !visted[v] && graph[u][v] < node_query[v]) //If u is connected to v & v jas not been visited & the edge weight connecting u and v is less than INF
             {
-                parent[v] = u, next_branch[v] = graph[u][v];
+               MST[v] = u, node_query[v] = graph[u][v]; //Adds the current node to the MST and adds the nodes connected to it to the query
             }
         }
     }
-
-    printMST(parent, graph);
+    printMST(MST, graph);
 }
 
 int main() {
     // Initializing the graph with edge weights
-    vector<vector<int>> graph = { { 0, 15,  0,  6, 2,  0 },
-                                  { 3,  0,  7,  0, 0,  0 },
-                                  { 0, 13,  0, 21, 7, 12 },
-                                  { 8,  8,  0,  0, 9,  7 },
-                                  { 0, 10, 12,  0, 0,  0 },
-                                  { 1, 15,  7,  3, 9,  0 } };
+    vector<vector<int>> graph = { { 0, 15,  0,  6,  0,  1 },
+                                  {15,  0, 13,  8,  0, 15 },
+                                  { 0, 13,  0, 21, 12,  7 },
+                                  { 6,  8, 21,  0,  9,  3 },
+                                  { 0,  0, 12,  9,  0,  9 },
+                                  { 1, 15,  7,  3,  9,  0 } };
 
     // Print the solution
     prim_algo(graph);
